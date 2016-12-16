c5ad147f04b250debfa9c6c4d907f02de653970a
crhilditch20 <crhilditch20@gmail.com>
2016-12-16 10:56:01 +0000

    create and test new unicorn class (extends creature class)

--- FantasyZoo/app/src/test/java/com/example/user/fantasyzoo/UnicornTest.java
+++ FantasyZoo/app/src/test/java/com/example/user/fantasyzoo/UnicornTest.java
@@ -1,7 +1,38 @@
 package com.example.user.fantasyzoo;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by user on 15/12/2016.
  */
 public class UnicornTest {
+
+    Unicorn unicorn;
+
+    @Before
+    public void before(){
+        unicorn = new Unicorn("Rainbow");
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals("Rainbow", unicorn.getName());
+    }
+
+    @Test
+    public void testCanFly(){
+        assertEquals("ascends gracefully into the air", unicorn.fly());
+    }
+
+    @Test
+    public void testCanSleep(){
+        assertEquals("Zzzzz....", unicorn.sleep());
+    }
+
+    @Test
+    public void testCanEat(){
+        assertEquals("Nom nom nom...", unicorn.eat());
+    }
 }
